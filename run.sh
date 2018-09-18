8c5224506220ab9d97ed238ecccea2d0246c8fac
automationgeek <jf.tremblay@gmail.com>
2018-09-18 19:46:44 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find /root/.polyglot/nodeserver/ -name 'requirements.txt' -exec pip3 install -r {} \;
-sh /opt/udi-polyglotv2/polyglot-v2-linux-x64
+# find /root/.polyglot/nodeserver/ -name 'requirements.txt' -exec pip3 install -r {} \;
+exec /opt/udi-polyglotv2/polyglot-v2-linux-x64
